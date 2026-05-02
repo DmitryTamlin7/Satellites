9b22c664f84abd8e2d1eac44f27abc4936e85e71
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2026-05-02 18:31:23 +0300

    Подготовка окружения

---
+++ telemetry-service/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+}
+
+group = "telemetry"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
